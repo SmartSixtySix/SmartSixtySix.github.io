35c85d464ec6f4fc52fe10f9b538cf23e738587c
Jeff McBride <jeff@geeklearn.com>
2018-01-30 18:01:42 -0500

    Removed unnecessary files.

--- php/qrredirectmapsgoogle.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-   header( 'Location: https://maps.google.com/maps?q=Smart+Sixty+Six&hl=en&ll=27.88248,-82.722073&spn=0.039754,0.084543&sll=27.894391,-82.718039&sspn=0.043012,0.084543&hq=Smart+Sixty+Six&t=m&z=14' ) ;
-
-?>
